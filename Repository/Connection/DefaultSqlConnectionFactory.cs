c0c00820d853ec871e09d5557bfedd56466bd603
FelipeSutter <lipeps61@gmail.com>
2024-03-20 17:52:51 -0300

    feat: Adicionando o início da parte de conexões

---
+++ Repository/Connection/DefaultSqlConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Contracts;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Repository.Connection
+{
+    public class DefaultSqlConnectionFactory : IConnectionFactory
+    {
+        public IDbConnection Connection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
